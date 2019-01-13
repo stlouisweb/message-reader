e7a05f5c0e3ffa61f0d01a8a457be9219d1ee835
Jeremy Plack <stlouisweb@gmail.com>
2019-01-13 10:29:38 -0600

    sets up json message reader script.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const fs = require("fs");
+const URL = require("url").URL;
+
+const messagesFileUrl = new URL(`file:///${__dirname}/messages/messages.json`);
+
+try {
+  const file = fs.readFileSync(messagesFileUrl, "utf-8");
+  const jsonData = JSON.parse(file);
+  const messages = jsonData.messages;
+  messages.forEach(message => console.info(message));
+} catch (err) {
+  if (err.code === "ENOENT") {
+    console.error("No messages found, try adding or pulling the messages subtree repo.");
+  } else {
+    console.error(err);
+  }
+}
